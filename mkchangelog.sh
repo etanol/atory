9c0f362439c96a35e0770ed4f055d75d9a27c87e
PXC Grup 26 <pxc26@fib.upc.edu>
2006-04-16 23:05:11 +0200

    Dotación de mayor flexibilidad a la hora de ejecutar la utilidad cvs2cl. Esto no tiene ninguna relevancia en absoluto, son sólo manías personales. [Isaac]

--- mkchangelog.sh
+++ mkchangelog.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 
-# Utilidad para invocar el script cvs2cl. Con distribución basada en Debian:
+# Utilidad para invocar el script cvs2cl. La gracia de este script es que
+# intenta encontrar el script en varios lugares distintos antes de fallar. Esté
+# donde esté, es importante que el script tenga el permiso de ejecución
+# activado.
+#
+# Con distribución basada en Debian:
 #
 # apt-get install cvs2cl
 #
@@ -11,5 +16,29 @@
 # $Revision$
 
 rm -f ChangeLog*
-cvs2cl -S -P -b -T --show-dead 
+
+cvs2cl_arguments='-S -P -b -T --show-dead'
+
+jare="$IFS"
+IFS=:
+
+for dir in $PATH:$HOME:$HOME/bin:.; do
+
+    # Probamos el nombre sin extensión (como en Debian)
+    if [ -x $dir/cvs2cl ]; then
+        # Restauramos el $IFS por si acaso
+        IFS="$jare"
+        $dir/cvs2cl $cvs2cl_arguments
+        exit
+    fi
+
+    # Probamos con la extensión (descargado de la web)
+    if [ -x $dir/cvs2cl.pl ]; then
+        # Aquí hacemos exactamente lo mismo
+        IFS="$jare"
+        $dir/cvs2cl.pl $cvs2cl_arguments
+        exit
+    fi
+
+done
 

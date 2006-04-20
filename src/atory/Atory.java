e965f56c06faf06f1b1fafdf24d7cd1d842c2347
PXC Grup 26 <pxc26@fib.upc.edu>
2006-04-20 23:46:20 +0200

    Creado el esqueleto de directorios que componen el paquete atory. Las clases pertenecientes a cada subpaquete deberán ubicarse en el directorio correspondiente. En cada uno de estos directorios existe un Makefile para poder compilar desde ese mismo directorio con facilidad, bastará con ejecutar "make".

---
+++ src/atory/Atory.java
@@ -0,0 +1,18 @@
+/*
+ * Atory.java - Clase principal.
+ *
+ * Esta es la clase que arranca todo el tinglado.
+ *
+ * $Revision$
+ */
+package atory;
+
+public class Atory {
+
+    public static void main (String args[])
+    {
+
+    }
+
+}
+

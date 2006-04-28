6d85e36293f599b35ed31c6918922045a0480397
PXC Grup 26 <pxc26@fib.upc.edu>
2006-04-28 09:11:39 +0200

    Resto ficheros para el parser

---
+++ src/atory/xml/Fichero.java
@@ -0,0 +1,68 @@
+import java.lang.String;
+
+//queda por decidir si los sets son void o int segun el tratamiento de errores
+
+public class Fichero
+{
+	String nombre;
+	String md5; //falta mirar si hi ha algun tipus especial
+	float tamano;
+	String comentario;
+
+	public Fichero()
+	{
+	   nombre = "";
+	   md5 = "";
+	   tamano = 0;
+	   comentario = "";
+	}
+
+	public Fichero(String nom, String enc, float tam, String com)
+	{
+		nombre = nom;
+		md5 = enc;
+		tamano = tam;
+		comentario = com;
+	}
+
+	public String getNombre()
+	{
+	    return nombre;
+	}
+
+	public void setNombre(String nom)
+	{
+        nombre = nom;      
+	}
+
+	public String getMd5()
+	{
+		return md5;
+	}
+
+	public void setMd5(String enc)
+	{
+		md5 = enc;      
+	}
+
+	public float getTamano()
+	{
+		return tamano;
+	}
+
+	public void setTamano(float tam)
+	{
+		tamano = tam;      
+	}
+
+	public String getComentario()
+	{
+		return comentario;
+	}
+
+	public void setComentario(String com)
+	{
+		comentario = com;      
+	}
+
+}
---
+++ src/atory/xml/JPListaFicheros.java
@@ -0,0 +1,26 @@
+import org.xmlpull.v1.XmlPullParserException;
+import java.util.Vector;
+import java.io.IOException;
+
+public class JPListaFicheros
+{
+	public static void main (String args[])
+		throws XmlPullParserException, IOException
+	{
+        ListaFicheros lf = new ListaFicheros("cosarara.xml");
+		Vector v = lf.getListaFicheros();
+		int n = v.size();
+		Fichero f;
+
+		for(int i=0;i<n;i++)
+		{
+            f = (Fichero)v.elementAt(i);
+			
+			System.out.print("Nombre del archivo: " + f.getNombre() + " ");
+			System.out.print("Md5: " + f.getMd5() + " ");
+			System.out.println("Tamano: " + f.getTamano() + " ");
+			System.out.println("Comentario del fichero: " + f.getComentario()+ "\n");
+		}
+		
+	}
+}
